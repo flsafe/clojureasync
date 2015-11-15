ace895785b7f02670b479d0501f6afcdaede238e
Frank Licea <francisco.licea@gmail.com>
2015-11-15 16:49:57 -0600

    messing with async

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to playsyc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/playsyc/core.clj
@@ -0,0 +1,9 @@
+(ns playsyc.core
+  (:require [clojure.core.async
+             :as a
+             :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]]))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
