58f214b740cc8e7c6bd292b11f82a15f405ff317
Frank Licea <francisco.licea@gmail.com>
2015-11-15 17:00:19 -0600

    alts

--- src/playsyc/core.clj
+++ src/playsyc/core.clj
@@ -3,6 +3,79 @@
              :as a
              :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]]))
 
+
+(defn hot-dog-machine
+  []
+  (let [in (chan)
+        out (chan)]
+    (go (<! in)
+        (>! out "hotdog"))
+    [in out]))
+
+(defn hot-dog-machine-v2
+  [hot-dog-count]
+  (let [in (chan)
+        out (chan)]
+    (go (loop [hc hot-dog-count]
+          (if (> hc 0)
+            (let [input (<! in)]
+              (if (= input 3)
+                (do (>! out "hot dog")
+                    (recur (dec hc)))
+                (do (>! out "whilted lettuce")
+                    (recur hc))))
+            (do (close! in)
+                (close! out)))))
+    [in out]))
+
+(let [[in out] (hot-dog-machine)]
+  (>!! in "pocket lint")
+  (<!! out))
+
+(let [[in out] (hot-dog-machine-v2 2)]
+  (>!! in "pocket link")
+  (println (<!! out))
+
+  (>!! in 3)
+  (println (<!! out))
+
+  (>!! in 3)
+  (println (<!! out))
+  
+  (>!! in 3)
+  (println  (<!! out)))
+
+(let [c1 (chan)
+      c2 (chan)
+      c3 (chan)]
+  (go (>! c2 (clojure.string/upper-case (<! c1))))
+  (go (>! c3 (clojure.string/reverse (<! c2))))
+  (go (println (<! c3)))
+  (>!! c1 "redrum"))
+
+(defn upload
+  [headshot c]
+  (go (Thread/sleep (rand 100))
+      (>! c headshot)))
+
+(let [c1 (chan)
+      c2 (chan)
+      c3 (chan)]
+  (upload "serious.jpg" c1)
+  (upload "fun.jpg" c2)
+  (upload "sassay.jpg" c3)
+  (let [[headshot c] (alts!! [c1 c2 c3 (timeout 20)])]
+    (if headshot
+      (println "Sending headshot notif for" headshot)
+      (println "Timed out"))))
+
+(let [c1 (chan)
+      c2 (chan)]
+  (go (<! c2))
+  (let [[value channel] (alts!! [c1 [c2 "puts!"]])]
+    (print value)
+    (= channel c2)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
